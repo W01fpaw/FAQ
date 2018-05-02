ddb8a6ddbd49968c77bc68cd8e066795d689e98d
W01fpaw <wsarchett@comcast.net>
2018-05-02 08:23:54 -0700

    Fixing more code that accidentally broke while setting up final in a different project.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -21,8 +22,11 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+
     public function index()
     {
-        return view('home');
+        $user = Auth::user();
+        $questions = $user->questions()->paginate(6);
+        return view('home')->with('questions', $questions);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -38,6 +38,4 @@ Route::delete('/questions/{question_id}/answer/{answer_id}', 'AnswerController@d
 Route::resources([
     'question' => 'QuestionController',
 ]);
-Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
